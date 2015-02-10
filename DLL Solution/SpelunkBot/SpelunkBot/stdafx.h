e7364042caec644b6ace4ca261727b8a32a2c66d
James Tatum <jtatum67@gmail.com>
2015-02-10 23:08:00 +0000

    James - C++ bot is in! You can code away in the 'Update' function of the new Bot DLL. Hopefully ti'll remember that I changed the build path, so once you compile the DLL you just have to launch the game. Let me know if anything is wanted/missing.

---
+++ DLL Solution/SpelunkBot/SpelunkBot/stdafx.h
@@ -0,0 +1,15 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS 
+
+#include <windows.h>
+#include <atlbase.h>
+#include <atlstr.h>
