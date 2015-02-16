e4a3b5076584ba52feb1f9d7c275296cae6c2162
James Tatum <100211640@unimail.derby.ac.uk>
2015-02-16 17:41:41 +0000

    James - Changed 'SpelunkBot' to 'Bot' so it's easier to Identify

---
+++ DLL Solution/Bot/SpelunkBot/stdafx.h
@@ -0,0 +1,48 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS 
+
+#include <windows.h>
+#include <atlbase.h>
+#include <atlstr.h>
+#include <iostream>
+
+enum TerrainType
+{
+	spEmptyNode = 0,
+	spStandardTerrain,
+	spLadder,
+	spEntrance,
+	spExit,
+	spSacAltar,
+	spArrowTrapRight,
+	spArrowTrapLeft,
+	spIsInShop,
+	spIce,
+	spSpike,
+	spSpearTrap,
+	spGameEntrance
+};
+
+enum Collectable
+{
+	spGoldBar = 1,
+	spGoldBars,
+	spEmerald,
+	spEmeraldBig,
+	spSapphire,
+	spSapphireBig,
+	spRuby,
+	spRubyBig,
+	spDiamond,
+	spGoldNugget,
+	spGoldChunk,
+};
